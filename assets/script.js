e15e03a60db280b36a840c0531056d39a42f1b05
RAvil <russian82@mail.com>
2019-12-20 18:26:02 -0500

    Fixed city bottoms

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
-    var city = "odessa";
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=246a9515c97ecb38ae4f2f7717982a0e";
-    $.ajax({
-        url: queryURL,
-        method:"GET"
-    }).then(function (response) {
-        console.log(response)
-    });
+    // var city = "odessa";
+    // var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=246a9515c97ecb38ae4f2f7717982a0e";
+    // $.ajax({
+    //     url: queryURL,
+    //     method:"GET"
+    // }).then(function (response) {
+    //     console.log(response)
+    // });
 
 
     var city = "richmond";
@@ -16,17 +16,58 @@ $(document).ready(function () {
         method:"GET"
     }).then(function (response) {
         console.log(response)
+        for ( var i = 6; i < response.list.length; i += 8) {
+            console.log(response.list[i])
+        }
     });
 
 
-    var city = "odessa";
-    var queryURLUV = "http://https://api.openweathermap.org/data/2.5/uvi?q=" + city + "&units=imperial&appid=246a9515c97ecb38ae4f2f7717982a0e";
-    $.ajax({
-        url: queryURLUV,
-        method:"GET"
-    }).then(function (response) {
-        console.log(response)
-    });
+    // var city = "odessa";
+    // var queryURLUV = "http://https://api.openweathermap.org/data/2.5/uvi?q=" + city + "&units=imperial&appid=246a9515c97ecb38ae4f2f7717982a0e";
+    // $.ajax({
+    //     url: queryURLUV,
+    //     method:"GET"
+    // }).then(function (response) {
+    //     console.log(response)
+    // });
+//     // create event when click search button
+//     $("#search_city").on("click", function(event){
+//         event.preventDefault();
+//         var city = $("#city_name").val()
+//     });
+
+//     // appending search of cities
+//     var citiesList = [];
+//     function citiesSearch(); {
+//         for (var i = 0; i < citiesList.length; i++);
+//         var citiesButton = $("<button>");
+//         citiesButton.text(citiesList[i]);
+//         $(".cities").append(citiesButton)
+// };
+// citiesSearch();
+var cities = [];
+function renderButtons() {
+    $(".cities-view").empty()
+    for (var i = 0; i < cities.length; i++) {
+        var a = $("<div>");
+        a.addClass("city btn btn-primary d-block mb-1 mr-auto ml-auto");
+        a.attr("data-name", cities[i]);
+        a.text(cities[i]);
+        $(".cities-view").prepend(a);
+    }
+
+};
+
+
+$("#search_city").on("click", function(event){
+    event.preventDefault();
+    var city = $("#add-city").val().trim();
+    cities.push(city);
+    renderButtons();
+});
+
+renderButtons();
+
 
 
 });
