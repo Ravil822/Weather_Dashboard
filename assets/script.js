9121f6242417a77b34d2fb0cb8f7068ecca46a71
RAvil <russian82@mail.com>
2019-12-26 23:05:18 -0500

    need work with local storage and read me

--- assets/script.js
+++ assets/script.js
@@ -1,56 +1,25 @@
+function renderButtons() {
+    $(".cities-view").empty()
+    for (var i = 1; i < cities.length; i++) {
+        var a = $("<div>");
+        a.addClass("city btn btn-primary d-block mb-1 mr-auto ml-auto");
+        a.attr("data-name", cities[i]);
+        a.text(cities[i]);
+        $(".cities-view").prepend(a);
+    }
+
+};
+
+$("#search_city").on("click", function (event) {
+    event.preventDefault();
+    var city = $("#add-city").val().trim();
+    cities.push(city);
+    localStorage.setItem("City-list", JSON.stringify(cities));
+    renderButtons();
+});
 
 
 
-
-
-// var city = "odessa";
-// var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=246a9515c97ecb38ae4f2f7717982a0e";
-// $.ajax({
-//     url: queryURL,
-//     method:"GET"
-// }).then(function (response) {
-//     console.log(response)
-// });
-
-
-// var city = "richmond";
-// var queryURL5D = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=246a9515c97ecb38ae4f2f7717982a0e";
-// $.ajax({
-//     url: queryURL5D,
-//     method:"GET"
-// }).then(function (response) {
-//     console.log(response)
-//     for ( var i = 6; i < response.list.length; i += 8) {
-//         console.log(response.list[i])
-//     }
-// });
-
-
-// var city = "odessa";
-// var queryURLUV = "http://https://api.openweathermap.org/data/2.5/uvi?q=" + city + "&units=imperial&appid=246a9515c97ecb38ae4f2f7717982a0e";
-// $.ajax({
-//     url: queryURLUV,
-//     method:"GET"
-// }).then(function (response) {
-//     console.log(response)
-// });
-//     // create event when click search button
-//     $("#search_city").on("click", function(event){
-//         event.preventDefault();
-//         var city = $("#city_name").val()
-//     });
-
-//     // appending search of cities
-//     var citiesList = [];
-//     function citiesSearch(); {
-//         for (var i = 0; i < citiesList.length; i++);
-//         var citiesButton = $("<button>");
-//         citiesButton.text(citiesList[i]);
-//         $(".cities").append(citiesButton)
-// };
-// citiesSearch();
-
-// Set cities buttons
 var cities = [""];
 var date = moment();
 function displayWheater() {
@@ -97,8 +66,8 @@ function displayWheater() {
             method: "GET"
         }).then(function (response) {
             // append the index
-            var uvDiv = $("#uv_index");
-            var uv = "UV index: " + response.value;
+            var uvDiv = $("#uv_index").addClass("btn btn-danger");
+            var uv = "UV index: " + response.value
             uvDiv.append(uv);
 
         });
@@ -132,14 +101,32 @@ function displayWheater() {
             var d5 = moment().add(4,"d").format("MM/DD/YYYY")
             date5.append(d5);
 
+            // Day 1 content
+
+            $("#pic_1").empty()
+            var picIndex1 = response.list[4].weather[0].icon
+            var picURL1 = "https://openweathermap.org/img/w/" + picIndex1 + ".png"
+            var pic1 = $("<img>").attr("src", picURL1)
+            var picDiv1 = $("#pic_1")
+            picDiv1.append(pic1);
+
             $("#current-temp1").empty(); $("#current-humi1").empty() 
             var tempDiv1 = $("#current-temp1");
-            var temp1 = "Temp: " + response.list[6].main.temp + " °F"
+            var temp1 = "Temp: " + response.list[4].main.temp + " °F"
             tempDiv1.append(temp1)
 
             var humiDiv1 = $("#current-humi1");
-            var humi1 = "Humidity: " + response.list[6].main.humidity + "%";
+            var humi1 = "Humidity: " + response.list[4].main.humidity + "%";
             humiDiv1.append(humi1);
+
+            // Day 2 content
+
+            $("#pic_2").empty()
+            var picIndex2 = response.list[12].weather[0].icon
+            var picURL2 = "https://openweathermap.org/img/w/" + picIndex2 + ".png"
+            var pic2 = $("<img>").attr("src", picURL2)
+            var picDiv2 = $("#pic_2")
+            picDiv2.append(pic2);
 
             $("#current-temp2").empty(); $("#current-humi2").empty()
             var tempDiv2 = $("#current-temp2");
@@ -150,23 +137,50 @@ function displayWheater() {
             var humi2 = "Humidity: " + response.list[12].main.humidity + "%";
             humiDiv2.append(humi2);
 
+            // Day 3 content
+
+            $("#pic_3").empty()
+            var picIndex3 = response.list[20].weather[0].icon
+            var picURL3 = "https://openweathermap.org/img/w/" + picIndex3 + ".png"
+            var pic3 = $("<img>").attr("src", picURL3)
+            var picDiv3 = $("#pic_3")
+            picDiv3.append(pic3);
+
             $("#current-temp3").empty(); $("#current-humi3").empty()
             var tempDiv3 = $("#current-temp3");
-            var temp3 = "Temp: " + response.list[18].main.temp + " °F"
+            var temp3 = "Temp: " + response.list[20].main.temp + " °F"
             tempDiv3.append(temp3)
 
             var humiDiv3 = $("#current-humi3");
-            var humi3 = "Humidity: " + response.list[18].main.humidity + "%";
+            var humi3 = "Humidity: " + response.list[20].main.humidity + "%";
             humiDiv3.append(humi3);
+
+            // Day 4 content
+
+            $("#pic_4").empty()
+            var picIndex4 = response.list[28].weather[0].icon
+            var picURL4 = "https://openweathermap.org/img/w/" + picIndex4 + ".png"
+            var pic4 = $("<img>").attr("src", picURL1)
+            var picDiv4 = $("#pic_4")
+            picDiv4.append(pic4);
 
             $("#current-temp4").empty(); $("#current-humi4").empty()
             var tempDiv4 = $("#current-temp4");
-            var temp4 = "Temp: " + response.list[24].main.temp + " °F"
+            var temp4 = "Temp: " + response.list[28].main.temp + " °F"
             tempDiv4.append(temp4)
 
             var humiDiv4 = $("#current-humi4");
-            var humi4 = "Humidity: " + response.list[24].main.humidity + "%";
+            var humi4 = "Humidity: " + response.list[28].main.humidity + "%";
             humiDiv4.append(humi4);
+
+            // Day 5 content 
+
+            $("#pic_5").empty()
+            var picIndex5 = response.list[36].weather[0].icon
+            var picURL5 = "https://openweathermap.org/img/w/" + picIndex5 + ".png"
+            var pic5 = $("<img>").attr("src", picURL1)
+            var picDiv5 = $("#pic_5")
+            picDiv5.append(pic5);
 
             $("#current-temp5").empty(); $("#current-humi5").empty()
             var tempDiv5 = $("#current-temp5");
@@ -181,43 +195,11 @@ function displayWheater() {
             });
 
     });
-
-    // make another call to the api to get 5 days forecast
-
     
-
-
-
-
-
-
 };
 
-
-
-
-function renderButtons() {
-    $(".cities-view").empty()
-    for (var i = 0; i < cities.length; i++) {
-        var a = $("<button>");
-        a.addClass("city btn btn-primary d-block mb-1 mr-auto ml-auto");
-        a.attr("data-name", cities[i]);
-        a.text(cities[i]);
-        $(".cities-view").prepend(a);
-    }
-
-};
-
-$("#search_city").on("click", function (event) {
-    event.preventDefault();
-    var city = $("#add-city").val().trim();
-    cities.push(city);
-    renderButtons();
-});
 
 $(document).on("click", ".city", displayWheater);
-
-    // Create click event with api request
 
 
 
